233fe04ea9616647088e326f7507b22eb18c7441
vishalicious213 <vishalicious213@gmail.com>
2023-05-11 20:42:50 -0700

    removed button elements from JS. capturing them via nav event listener. added event handler for button click

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-const shopBtn = document.getElementById("shop-btn")
-const todoBtn = document.getElementById("to-do-btn")
-const noteBtn = document.getElementById("notes-btn")
+const nav = document.getElementById("nav")
 const main = document.getElementById("main")
 const title = document.getElementById("title")
 
 // ⬇️ EVENT LISTENERS ⬇️
 
+nav.addEventListener("click", function(e) {
+    const button = e.target.id
+    buttonClick(button)
+})
 // ⬇️ EVENT HANDLERS ⬇️
+
+function buttonClick(button) {
+    if (button === "shop-btn") {
+        console.log("Shopping")
+    }
+
+    if (button === "to-do-btn") {
+        console.log("To Do List")
+    }
+
+    if (button === "notes-btn") {
+        console.log("Notes")
+    }
+}
 
 // ⬇️ RENDER APP ⬇️
