c5a72660819a544d2138c637c67e7b06d92ef15c
vishalicious213 <vishalicious213@gmail.com>
2023-05-11 23:12:47 -0700

    imported push from firebase. pushing value in shopInput to firebase

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 // firebase settings
 const appSettings = {
@@ -53,6 +53,7 @@ function buttonClick(button) {
 function addToCart() {
     if (shopInput.value) {
         console.log(shopInput.value)
+        push(shoppingListInDB, shopInput.value)
     }
 }
 
