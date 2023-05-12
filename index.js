e0d33c83e85f353711219c21bad33b988aae4d83
vishalicious213 <vishalicious213@gmail.com>
2023-05-11 20:56:21 -0700

    drafted clearMain(). added draft content to shopping list

--- index.js
+++ index.js
@@ -26,14 +26,31 @@ function buttonClick(button) {
 
 // ⬇️ RENDER APP ⬇️
 
+function clearMain() {
+    main.innerHTML = ""
+}
+
 function renderShopping() {
     title.innerText = "Shopping List"
+    clearMain()
+
+    main.innerHTML = `
+        <ul id="shopping-list">
+            <li>item 1</li>
+            <li>item 2</li>
+            <li>item 3</li>
+        </ul>
+    `
 }
 
 function renderToDoList() {
     title.innerText = "To Do List"
+    clearMain()
 }
 
 function renderNotes() {
     title.innerText = "Notes"
+    clearMain()
 }
+
+renderShopping()
