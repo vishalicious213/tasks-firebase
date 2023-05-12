dc66bb2c152585c3e972e225f039b946fa15149b
vishalicious213 <vishalicious213@gmail.com>
2023-05-11 20:36:09 -0700

    grabbed main, title & buttons in JS

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Tasks")
+const shopBtn = document.getElementById("shop-btn")
+const todoBtn = document.getElementById("to-do-btn")
+const noteBtn = document.getElementById("notes-btn")
+const main = document.getElementById("main")
+const title = document.getElementById("title")
+
+// ⬇️ EVENT LISTENERS ⬇️
+
+// ⬇️ EVENT HANDLERS ⬇️
+
+// ⬇️ RENDER APP ⬇️
